d5087afff145c64d9e863871ae47e2da71ca2662
franciscoken <franciscokenandi@gmail.com>
2018-11-20 11:10:03 +0700

    added dummy validation process

--- src/Node/index.js
+++ src/Node/index.js
@@ -13,6 +13,10 @@ const client = new Client(config);
 
 const BASE_URL = "http://localhost:8060/"
 
+const paymentGateway = {
+    url:"http://localhost:8080/engine-rest/process-definition/key/dummy-process/start"
+}
+
 client.subscribe('search-room', async function({ task, taskService }) {
   const roomType = task.variable.get('room_type');
   const checkIn = task.variable.get('check_in');
@@ -62,7 +66,6 @@ client.subscribe('cancel-booking', async function({ task, taskService }) {
     try {
         let response = await axios.delete(BASE_URL+'invoice/'+invoiceId);
         bookingDetails = response.data;
-        console.log(typeof response.data);
         if (bookingDetails.length !== 0) {
             //Sets Payment status
             if(bookingDetails.paid === true){
@@ -73,7 +76,6 @@ client.subscribe('cancel-booking', async function({ task, taskService }) {
 
             //Calls cancel API
             cancelResponse = await axios.delete(BASE_URL+'invoice/'+invoiceId);
-            console.log(typeof cancelResponse.data);
             if(cancelResponse.data.success === true){
                 processVariables.set('cancelled', true);
             } else {
@@ -89,10 +91,17 @@ client.subscribe('cancel-booking', async function({ task, taskService }) {
     await taskService.complete(task, processVariables);
 });
 
-client.subscribe('send-payment-information', async function({ task, taskService }) {
+client.subscribe('request-payment-information', async function({ task, taskService }) {
+    const booking_id = task.variables.get('booking_id');
+    const price = task.variables.get('price');
+    const payer_name = task.variables.get('payer_name');
+    const payment_type = task.variables.get('payment_type');
     const processVariables = new Variables();
 
     try {
+        // let response = await axios.post(paymentGateway.url,{
+
+        // });
         //Calls Payment Gateway Service
     } catch(error) {
         console.log(error);
@@ -118,3 +127,26 @@ client.subscribe('update-invoice', async function({ task, taskService }) {
     await taskService.complete(task, processVariables);
 });
 
+client.subscribe('dummy-validation', async function({ task, taskService }) {
+    const price = task.variables.get('price');
+    const name = task.variables.get('name');
+    const type = task.variables.get('type');
+    const confirmation = task.variables.get('confirm');
+    const processVariables = new Variables();
+
+    var verdict = confirmation;
+
+    if(name === "reinhard"){
+        verdict = true;
+    } else if(price>5000000){
+        verdict = true;
+    }
+    console.log(verdict);
+
+    if(verdict){
+        processVariables.set('confirm',true);
+    } else {
+        processVariables.set('confirm',false);
+    }
+    await taskService.complete(task, processVariables);
+});
