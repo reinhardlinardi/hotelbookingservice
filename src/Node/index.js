250fcf26d369d669700c1767f581a34ec85d9238
franciscoken <franciscokenandi@gmail.com>
2018-11-24 18:22:15 +0700

    refactor and clean up functions, adding implementation for cancel

--- src/Node/index.js
+++ src/Node/index.js
@@ -1,22 +1,33 @@
+/*
+    Package Definition
+*/
 const axios = require('axios').create();
-const http = require('http');
-
+const fs = require('fs');
 const { Client, logger, Variables } = require('camunda-external-task-client-js');
+
+/*
+Configuration
+*/
+// Reading configuration file
+var conf;
+try{
+    conf = JSON.parse(fs.readFileSync('config.json','utf8'));
+} catch(error){
+    console.log(error);
+}
 
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
 //  - 'logger': utility to automatically log important events
-const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger };
-
+const client_config = { baseUrl: conf['client_url'], use: logger };
 // create a Client instance with custom configuration
-const client = new Client(config);
+const client = new Client(client_config);
+const BASE_URL = conf['entity_service_url'];
 
-const BASE_URL = "http://localhost:8060/"
-
-const paymentGateway = {
-    url:"http://localhost:8080/engine-rest/process-definition/key/validate-payment-task/start"
-}
-
+/* 
+    Implementation 
+*/
+// Order room process implementations
 client.subscribe('search-room', async function({ task, taskService }) {
   const roomType = task.variables.get('room_type');
   const checkIn = task.variables.get('check_in');
@@ -38,6 +49,7 @@ client.subscribe('search-room', async function({ task, taskService }) {
         processVariables.set('room_id',room[0]);
     } else {
         processVariables.set('found', 'false');
+        console.log(response.data.message)
     }
   } catch(error) {
     console.log(error);
@@ -46,7 +58,6 @@ client.subscribe('search-room', async function({ task, taskService }) {
 });
 
 client.subscribe('create-invoice', async function({ task, taskService }) {
-  const roomType = task.variables.get('room_type');
   const customerId = task.variables.get('user_id');
   const checkIn = task.variables.get('check_in');
   const checkOut = task.variables.get('check_out');
@@ -81,6 +92,7 @@ client.subscribe('get-room-price', async function({ task, taskService }) {
     const processVariables = new Variables();
 
     processVariables.set('payment_type','credit');
+
     //Get Room Price
     try{
         let roomResponse = await axios.get(BASE_URL+'room/'+room_id);
@@ -107,60 +119,12 @@ client.subscribe('get-payer-name', async function({ task, taskService }) {
                 payer_name = custResponse.data.data.name;
                 processVariables.set('payer_name',payer_name);
             } else {
-                console.log('returns 0 data');
+                console.log('returns no data');
             }
         } else {
             console.log(custResponse.data.message);
         }
     } catch(error){
-        console.log(error);
-    }
-    await taskService.complete(task, processVariables);
-});
-
-client.subscribe('cancel-booking', async function({ task, taskService }) {
-    const invoiceId = task.variables.get('invoice_id');
-    const processVariables = new Variables();
-
-    try {
-        let response = await axios.delete(BASE_URL+'invoice/'+invoiceId);
-        bookingDetails = response.data;
-        if (bookingDetails.length !== 0) {
-            //Sets Payment status
-            if(bookingDetails.paid === true){
-                processVariables.set('paid',true);
-            } else {
-                processVariables.set('paid',false);
-            }
-
-            //Calls cancel API
-            cancelResponse = await axios.delete(BASE_URL+'invoice/'+invoiceId);
-            if(cancelResponse.data.success === true){
-                processVariables.set('cancelled', true);
-            } else {
-                console.log(cancelResponse.data.message);
-                processVariables.set('cancelled',false);
-            }
-        } else {
-            processVariables.set('cancelled', false);
-        }
-    } catch(error) {
-        console.log(error);
-    }
-    await taskService.complete(task, processVariables);
-});
-
-client.subscribe('request-payment-confirmation', async function({ task, taskService }) {
-    const booking_id = task.variables.get('booking_id');
-    const price = task.variables.get('price');
-    const payer_name = task.variables.get('payer_name');
-    const payment_type = task.variables.get('payment_type');
-    const processVariables = new Variables();
-
-    try {
-        //Calls Payment Gateway Service
-        processVariables.set('approved',true);
-    } catch(error) {
         console.log(error);
     }
     await taskService.complete(task, processVariables);
@@ -189,26 +153,85 @@ client.subscribe('update-invoice', async function({ task, taskService }) {
     await taskService.complete(task, processVariables);
 });
 
-client.subscribe('dummy-validation', async function({ task, taskService }) {
-    const price = task.variables.get('price');
-    const name = task.variables.get('name');
-    const type = task.variables.get('type');
-    const confirmation = task.variables.get('confirm');
+// Implementations of validate payment process
+client.subscribe('request-payment-confirmation', async function({ task, taskService }) {
     const processVariables = new Variables();
 
-    var verdict = confirmation;
-
-    if(name === "reinhard"){
-        verdict = true;
-    } else if(price>5000000){
-        verdict = true;
-    }
-    console.log(verdict);
-
-    if(verdict){
-        processVariables.set('confirm',true);
-    } else {
-        processVariables.set('confirm',false);
+    //Calls Payment Gateway Service
+    try {
+        processVariables.set('approved',true);
+    } catch(error) {
+        console.log(error);
     }
     await taskService.complete(task, processVariables);
 });
+
+//Cancel Booking Process Implementations
+client.subscribe('get-invoice', async function({ task, taskService }) {
+    const invoiceId = task.variables.get('invoice_id');
+    const processVariables = new Variables();
+
+    try {
+        let response = await axios.get(BASE_URL+'invoice/'+invoiceId);
+        bookingDetails = response.data;
+        if (bookingDetails.success) {
+            processVariables.set('exist',true);
+            //Sets Payment status
+            if(bookingDetails.data.paid === true){
+                processVariables.set('paid',true);
+            } else {
+                processVariables.set('paid',false);
+            }
+        } else {
+            processVariables.set('exist', false);
+        }
+    } catch(error) {
+        console.log(error);
+    }
+    await taskService.complete(task, processVariables);
+});
+
+client.subscribe('cancel-booking', async function({ task, taskService }) {
+    const invoiceId = task.variables.get('invoice_id');
+    const processVariables = new Variables();
+
+    //Calls cancel API
+    try {
+        let cancelResponse = await axios.delete(BASE_URL+'invoice/'+invoiceId);
+        if(cancelResponse.data.success === true){
+            processVariables.set('cancelled', true);
+        } else {
+            console.log(cancelResponse.data.message);
+            processVariables.set('cancelled',false);
+        }
+    } catch(error) {
+        console.log(error);
+    }
+    await taskService.complete(task, processVariables);
+});
+
+
+//Implementation of dummy validation process
+// client.subscribe('dummy-validation', async function({ task, taskService }) {
+//     const price = task.variables.get('price');
+//     const name = task.variables.get('name');
+//     const type = task.variables.get('type');
+//     const confirmation = task.variables.get('confirm');
+//     const processVariables = new Variables();
+
+//     var verdict = confirmation;
+
+//     if(name === "reinhard"){
+//         verdict = true;
+//     } else if(price>5000000){
+//         verdict = true;
+//     }
+//     console.log(verdict);
+
+//     if(verdict){
+//         processVariables.set('confirm',true);
+//     } else {
+//         processVariables.set('confirm',false);
+//     }
+//     await taskService.complete(task, processVariables);
+// });
